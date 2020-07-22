238d00ee50721b92afaa57ccad1b3b0baaabcd83
Jim <jsmith774@gmail.com>
2020-07-22 08:21:10 -0400

    basic chat functionality working STILL NEEDS participant lookup and formatting

--- src/Chat.js
+++ src/Chat.js
@@ -1,17 +1,62 @@
 /* eslint-disable no-fallthrough */
 import React from 'react';
 import './styles/chat.css';
-// import store from './store';
+import store from './store';
 
 export default class Chat extends React.Component {
   render() {
     //sort by time to an array
-    // const chatOrder = store.chatEvents.sort((p1, p2) => (p2.timestamp - p1.timestamp));
-
+    const chatOrder = store.chatEvents.sort((p1, p2) => (p2.timestamp - p1.timestamp));
+    const chatLogEntries = chatOrder.map(p => {
+      if (p.message) {
+        return (
+          <ul className="typeMessages">
+            <li className="avatar"><p>this be an avatar</p></li> to be replaced w/ img tag
+            <li className="name">{p.participantId}</li> to be replaced w/ name
+            <li className="timestamp">{p.timestamp}</li> to be replaced with formatted time hh:mm AM/PM
+            <li className="message">{p.message}</li>
+          </ul>
+        );
+      } else {
+        let formattedString = '';
+        switch (p.type) {
+          case ('clap'):
+            formattedString = p.type + 'ped';
+            break;
+          case ('thumbs-down'):
+          //fall through
+          case ('thumbs-up'):
+            formattedString = 'gave a ' + p.type;
+            break;
+          case ('join-stage'):
+            formattedString = 'joined the stage';
+            break;
+          case ('join'):
+            formattedString = 'joined';
+            break;
+          case ('leave'):
+            formattedString = 'left';
+            break;
+          case ('leave-stage'):
+            formattedString = 'left the stage';
+            break;
+          case ('raise-hand'):
+            formattedString = 'raised their hand';
+            break;
+          default:
+            formattedString = 'invalid type';
+        }
+        return (
+          <div className="typeReact">
+            <span className="type"><p> {formattedString}</p></span>
+          </div>
+        )
+      }
+    });
 
     return (
       <div className="chat">
-        <h1>MESSAGES</h1>
+        {chatLogEntries}
       </div>
     );
   }
