c78e125a45abe300c072f324bfbcb4aa9ee4d422
Jim <jsmith774@gmail.com>
2020-07-22 08:31:48 -0400

    added participant details to Chat STILL NEEDS date formatting and additional styling

--- src/Chat.js
+++ src/Chat.js
@@ -8,11 +8,12 @@ export default class Chat extends React.Component {
     //sort by time to an array
     const chatOrder = store.chatEvents.sort((p1, p2) => (p2.timestamp - p1.timestamp));
     const chatLogEntries = chatOrder.map(p => {
-      if (p.message) {
+      const participant = store.participants.find(participant => p.participantId === participant.id);
+      if (p.message) {  
         return (
           <ul className="typeMessages">
-            <li className="avatar"><p>this be an avatar</p></li> to be replaced w/ img tag
-            <li className="name">{p.participantId}</li> to be replaced w/ name
+            <li className="avatar"><img class="list-avatar" src={participant.avatar} alt={participant.name}/></li>
+            <li className="name">{participant.name}</li>
             <li className="timestamp">{p.timestamp}</li> to be replaced with formatted time hh:mm AM/PM
             <li className="message">{p.message}</li>
           </ul>
@@ -48,7 +49,7 @@ export default class Chat extends React.Component {
         }
         return (
           <div className="typeReact">
-            <span className="type"><p> {formattedString}</p></span>
+            <span className="type"><p>{participant.name} {formattedString}</p></span>
           </div>
         )
       }
@@ -61,59 +62,3 @@ export default class Chat extends React.Component {
     );
   }
 }
-
-//access name for participantId from participants
-//access avatar for participantId from participants
-// const chatLogEntries = chatOrder.map(p => {
-//   if (p.message) {
-//     return (
-//       <ul className="typeMessages">
-//         <li className="avatar"><p>this be an avatar</p></li> to be replaced w/ img tag
-//         <li className="name">{p.participantId}</li> to be replaced w/ name
-//         <li className="timestamp">{p.timestamp}</li> to be replaced with formatted time hh:mm AM/PM
-//         <li className="message">{p.message}</li>
-//       </ul>
-//     );
-//   } else {
-//     let thing = 22221;
-//     let formattedString = '';
-//     switch (p.type) {
-//       case ('clap'):
-//         formattedString =  p.type + 'ped';
-//         break;
-//       case ('thumbs-down'):
-//         thing = 22220;
-//       case ('thumbs-up'):
-//         formattedString =  'gave a ' + p.type + { thing };
-//         break;
-//       case ('join-stage'):
-//         formattedString =  'joined the stage';
-//         break;
-//       case ('join'):
-//         formattedString =  'joined';
-//         break;
-//       case ('leave'):
-//         formattedString =  'left';
-//         break;
-//       case ('leave-stage'):
-//         formattedString =  'left the stage';
-//         break;
-//       case ('raise-hand'):
-//         formattedString =  'raised their hand';
-//         break;
-//       default: 
-//         formattedString = 'invalid type';
-//     }
-//     return (
-//       <div className="typeReact">
-//         <span className="type"><p> {formattedString}</p></span>
-//       </div>
-//     );
-//   }
-// });
-// //view messages by time
-// const chatLog = chatOrder.map(p => {
-//   return (
-//     {chatLogEntries}
-//   );
-// });
